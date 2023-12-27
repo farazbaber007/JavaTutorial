f49eb3e3d31cd204000edb83d078d945bdf5df1b
farazbaber007 <farazbaber007@gmail.com>
2023-12-28 01:57:22 +0500

    zeeshanhanif
    Java Script
    prerequisites

---
+++ zeeshanhanif/Lec01/index.js
@@ -0,0 +1,46 @@
+// window.alert("This is window.alert popup")
+// alert(23);
+// alert("Hello Moto")
+// document.write("Hello document");
+// document.write(2+8);
+
+// var nationality = "Pakistani";
+// alert(typeof(nationality));
+
+ var age = 25;
+ alert(age);
+ document.write(age);
+ console.log(age);
+age = 65
+document.write(age);
+alert(age);
+console.log(age);
+
+//  alert(typeof(age));
+//  document.write(age);
+//  var isFeePaid= true;
+//  alert(typeof(isFeePaid));
+// document.write(isFeePaid);
+ var weight = 60.55;
+// alert(weight);
+// document.write(weight);
+
+
+// for serverside programing shows in developer tool in console tab
+console.log(weight);
+
+
+// console.log("Hellow Motto")
+// var marks; //--declaration
+// marks = 98;
+
+// var  name = "Mark"
+// var patioNality1 = 'US';
+
+
+// var Name1 =  mark;
+// Name1 = 25;
+// Name1 = ture;
+// Name1 = "what is this?"
+
+
---
+++ zeeshanhanif/lec02/index.js
@@ -0,0 +1,4 @@
+alert("Test Lec 02");
+
+
+document.write("Lecture Two 'data type' ")
