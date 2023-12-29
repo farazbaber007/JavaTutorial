ee66b6a15b790acdb26f889dffb934a6f79e52a4
farazbaber007 <farazbaber007@gmail.com>
2023-12-29 23:31:14 +0500

    lec03
    updated

---
+++ zeeshanhanif/lec03/index.js
@@ -0,0 +1,54 @@
+alert("Hello Motto");
+
+var a = 2;
+var b = 2;
+
+var d = "name";
+var e = true;
+var f;
+var h = null;
+var i, j, k;
+
+
+alert("var a is a " + typeof(a) ) ; 
+document.write("var a is a " + typeof(a)); 
+alert("var b is a " + typeof(b));
+document.write("var b is b " + typeof(b));
+
+document.write("var d is a  " + typeof(d)); 
+document.write("var e is a  " + typeof(e)); 
+document.write("var f is a " + typeof(f)); 
+document.write("var h is a " + typeof(h)); 
+alert("var h is a " + typeof(h));
+document.write("var i is a " + typeof(i));
+document.write("var j is a " + typeof(j));
+document.write("var k is a " + typeof(k));
+
+
+
+
+document.write(a * b); 
+document.write(a / b);
+document.write(a - b);
+document.write(a % b);
+document.write(a * b);
+document.write(a ** b); 
+
+
+
+document.write(a * b); 
+document.write(a / b);
+document.write(a - b);
+document.write(a % b);
+document.write(a * b);
+document.write(a ** b);
+
+
+var myName = [Faraz, Ayaz, Asim, Atif, Saqib];
+
+for ( i = 0; 
+      i < myName.length;
+      i++) {
+        alert("Names are: " + myName[i]);
+      }
+
