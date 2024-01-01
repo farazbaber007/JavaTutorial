50f90a0fa02f46a709bb87dbfe341c2b31a1efcc
farazbaber007 <farazbaber007@gmail.com>
2024-01-02 01:03:41 +0500

    lect04
    zeshan Hanif video No. 62
    https://www.youtube.com/watch?v=xdMSRiHj3d0&list=PLKvqnz8z1zWQdc0NSLknxmxBch5gLOqyo&index=63

---
+++ zeeshanhanif/lec04/index.js
@@ -0,0 +1,33 @@
+var question = "Your Name Please?";
+var defaultAnswer = "Faraz";
+var name = prompt(question, defaultAnswer);
+console.log("Name = " + name);
+
+
+var question1 = "what is your age?"
+var age = prompt(question1);
+var newAge = age + 5;
+console.log("New Age is: " + newAge );
+
+var a = "what are you learning? ";
+var b = prompt(a);
+console.log("learning: " + b);
+
+var introduction =  prompt("what is your Name?" );
+console.log(introduction);
+
+
+var introduction =  prompt("what is your Name?", "Faraz");
+console.log(introduction);
+
+var question1 = "what is your age?"
+var age = prompt(question1);
+var question2 = "class room name please";
+var room =  prompt(question2);
+var newAge = age + 5;
+var question3 = "is fee paid yes or no";
+var fee = prompt(question3);
+console.log("New Age is: " + age);
+console.log("room is : " + room )
+console.log("newAge is :" + newAge);
+console.log("is fee: "); 
